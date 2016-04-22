63d188906e3773c644263124933c2bb9c5b2e71c
Rajneesh Singh <rajneesh.singh@girnarsoft.com>
2016-04-22 17:17:11 +0530

    adding sql file

---
+++ db.sql
@@ -0,0 +1,12 @@
+
+CREATE TABLE IF NOT EXISTS `demos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(300) DEFAULT NULL,
+  `description` text NOT NULL,
+  `link` varchar(300) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+INSERT INTO `demos` (`id`, `title`, `description`, `link`) VALUES
+(1, 'Google', 'desc 1', 'http://google.com'),
+(2, 'Facebook', 'desc 2', 'http://facebook.com');
